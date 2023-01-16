c4d34c15ca4bccc5024d0ebae798320e6400f10e
AyoubTahir <ayoub.tahir.2021@gmail.com>
2023-01-16 21:58:24 +0100

    adding seachUsers resolver

--- src/graphql/resolvers/user.ts
+++ src/graphql/resolvers/user.ts
@@ -1,8 +1,41 @@
+import { ApolloError } from "apollo-server-core";
 import { createUsernameResponse, graphQLContext } from "../../util/types";
+import { User } from "@prisma/client";
 
 const resolvers = {
   Query: {
-    searchUsers: () => {},
+    searchUsers: async (
+      _: any,
+      args: { username: string },
+      context: graphQLContext
+    ): Promise<Array<User>> => {
+      const { username: searchedUsername } = args;
+      const { session, prisma } = context;
+
+      if (!session?.user) {
+        throw new ApolloError("Not authorized");
+      }
+
+      const {
+        user: { username: myUsername },
+      } = session;
+
+      try {
+        const users = await prisma.user.findMany({
+          where: {
+            username: {
+              contains: searchedUsername,
+              not: myUsername,
+              mode: "insensitive",
+            },
+          },
+        });
+        return users;
+      } catch (error: any) {
+        console.log("searchUsers error ", error);
+        throw new ApolloError(error?.message);
+      }
+    },
   },
   Mutation: {
     createUsername: async (
